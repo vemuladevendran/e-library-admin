ee0c9381d3e5718264f270c209652b3ae2d47135
devendran <devendranvemula@gmail.com>
2022-03-13 07:42:41 +0530

    clean up

--- src/app/pages/books/add-book/add-book.component.ts
+++ src/app/pages/books/add-book/add-book.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDatepicker } from '@angular/material/datepicker';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-book',
@@ -8,10 +9,15 @@ import { MatDatepicker } from '@angular/material/datepicker';
   styleUrls: ['./add-book.component.scss']
 })
 export class AddBookComponent implements OnInit {
+  @ViewChild('bookImage') fileInput: ElementRef<HTMLInputElement> | undefined;
+  selectedImagePreviewURL: any = "";
+  selectedFile: any;
   authors = ['deva', 'hari', 'charan'];
-  createBookForm: FormGroup
+  createBookForm: FormGroup;
+
   constructor(
     private fb: FormBuilder,
+    private sanitizer: DomSanitizer,
   ) {
     this.createBookForm = this.fb.group({
       image: [null],
@@ -29,4 +35,26 @@ export class AddBookComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // image change handle
+  handleFileSelection(): void {
+    const [file] = this.fileInput?.nativeElement?.files as any as File[];
+    if (file) {
+      this.selectedFile = file;
+      this.selectedImagePreviewURL = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file));
+    }
+  }
+  
+  // open file input
+  openFileSelectionDialog(): void {
+    this.fileInput?.nativeElement?.click();
+  }
+
+  // on submiting the form
+  async createBook(): Promise<void> {
+    try {
+      console.log(this.createBookForm.value);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
