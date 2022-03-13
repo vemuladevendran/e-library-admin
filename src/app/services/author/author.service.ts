77a7306f436f87394fd9626810fb29522db7dc1f
devendran <devendranvemula@gmail.com>
2022-03-13 14:56:42 +0530

    author services apis added

---
+++ src/app/services/author/author.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { SettingsService } from '../settings/settings.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorService {
+
+  constructor(
+    private http: HttpClient,
+    private settings: SettingsService
+  ) { }
+
+  // get authors
+  getAuthors(): Promise<any> {
+    const url = `${this.settings.API_BASE_URL}/author`;
+    return lastValueFrom(this.http.get(url));
+  }
+
+  // create new author
+  createAuthor(data: any): Promise<any> {
+    const url = `${this.settings.API_BASE_URL}/author`;
+    return lastValueFrom(this.http.post(url, data))
+  }
+
+  // delete author
+  deleteAuthor(id: string): Promise<any> {
+    const url = `${this.settings.API_BASE_URL}/author/${id}`;
+    return lastValueFrom(this.http.delete(url))
+  }
+}
