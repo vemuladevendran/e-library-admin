3b2dabe25a933c8a95b655fa7857b553a28ae72c
devendran <devendranvemula@gmail.com>
2022-03-16 17:04:27 +0530

    new route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
     children: [
       { path: 'authors', loadChildren: () => import('./pages/author/author.module').then(m => m.AuthorModule) },
       { path: 'books', loadChildren: () => import('./pages/books/books.module').then(m => m.BooksModule) },
+      { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
     ],
   },
   {
--- src/app/components/app-shell/sidenav-items.ts
+++ src/app/components/app-shell/sidenav-items.ts
@@ -1,5 +1,6 @@
 export const menuItems = [
     ['dashboard_customize', 'Dashboard', ['/dashboard']],
     ['library_books', 'Books', ['/books']],
-    ['person', 'Author', ['/authors']],
+    ['person', 'Admin', ['/admin']],
+    ['record_voice_over', 'Author', ['/authors']],
 ].map(([icon, text, path]) => ({ icon, text, path }));
