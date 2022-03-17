7eebdb4da98b29921e645e1a5b1dc71ee7fe9646
devendran <devendranvemula@gmail.com>
2022-03-17 14:11:03 +0530

    login design done

--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Toast } from 'ngx-toastr';
+import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  passwordHide = false;
-  constructor(private fb: FormBuilder) {
+  passwordHide = true;
+  invalidDetails = ''
+  constructor(
+    private fb: FormBuilder,
+    private loader: LoaderService,
+  ) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+
+  async login(): Promise<void> {
+    try {
+      console.log(this.loginForm.value);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.loader.hide();
+    }
   }
 
   ngOnInit(): void { }
