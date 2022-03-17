5c189a3fbd87c6387bd37184f605918f35b55030
devendran <devendranvemula@gmail.com>
2022-03-17 10:33:27 +0530

    admin module done

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AdminService } from 'src/app/services/admin/admin.service';
+import { LoaderService } from 'src/app/services/loader/loader.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  adminList: any[] = [];
+  constructor(
+    private adminServe: AdminService,
+    private loader: LoaderService,
+    private toast: ToastrService,
+  ) { }
 
-  constructor() { }
+  // get admin list
+
+  async getAdminsList(): Promise<void> {
+    try {
+      this.loader.show();
+      this.adminList = await this.adminServe.getAdmins();
+    } catch (error) {
+      console.log(error);
+      this.toast.error("Fail to load");
+    } finally {
+      this.loader.hide();
+    }
+  }
+
+  // delete admin
+  async deleteBook(id: any): Promise<void> {
+    const result = await Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    });
+
+    if (result.isConfirmed) {
+      try {
+        await this.adminServe.deleteAdmin(id);
+        this.toast.info('Deleted');
+        this.getAdminsList();
+      } catch (error) {
+        console.log(error, 'fail to delete');
+        this.toast.error('fail to delete')
+      }
+    }
+  }
 
   ngOnInit(): void {
+    this.getAdminsList();
   }
 
 }
