06a758832cc7474cfae1ec3ef2c9a8ece4d94ccd
devendran <devendranvemula@gmail.com>
2022-03-24 20:42:55 +0530

    admin security added

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AdminService } from 'src/app/services/admin/admin.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
+import { TokenService } from 'src/app/services/token/token.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -11,10 +12,12 @@ import Swal from 'sweetalert2';
 })
 export class AdminComponent implements OnInit {
   adminList: any[] = [];
+  myId: string = '';
   constructor(
     private adminServe: AdminService,
     private loader: LoaderService,
     private toast: ToastrService,
+    private token: TokenService
   ) { }
 
   // get admin list
@@ -23,6 +26,7 @@ export class AdminComponent implements OnInit {
     try {
       this.loader.show();
       this.adminList = await this.adminServe.getAdmins();
+      this.getMyDetails();
     } catch (error) {
       console.log(error);
       this.toast.error("Fail to load");
@@ -31,8 +35,22 @@ export class AdminComponent implements OnInit {
     }
   }
 
+  async getMyDetails(): Promise<void> {
+    try {
+      const data = await this.token.getUserName();
+      this.myId = data._id
+    } catch (error) {
+      console.log(error);
+      this.toast.error("Fail to load");
+    }
+  }
+
   // delete admin
   async deleteBook(id: any): Promise<void> {
+    if (id === this.myId) {
+      this.toast.error('Your are trying to delete your Id');
+      return;
+    }
     const result = await Swal.fire({
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
--- src/app/pages/materials/add-materials/add-materials.component.ts
+++ src/app/pages/materials/add-materials/add-materials.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BookService } from 'src/app/services/book/book.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
@@ -18,13 +19,15 @@ export class AddMaterialsComponent implements OnInit {
   createMaterialForm: FormGroup;
   booksCategory: any[] = [];
   zoomLevel = 0;
+  formData = new FormData();
 
   constructor(
     private fb: FormBuilder,
     private bookServe: BookService,
     private sanitizer: DomSanitizer,
     private loader: LoaderService,
-    private toast: ToastrService
+    private toast: ToastrService,
+    private router: Router
   ) {
     this.createMaterialForm = this.fb.group({
       title: ['', [Validators.required]],
@@ -52,7 +55,15 @@ export class AddMaterialsComponent implements OnInit {
     if (file) {
       this.selectedFile = file;
       this.selectedFilePreviewURL = file.name;
+      this.pdfPeviewFile = null;
     }
+  }
+
+  // remove selected file
+  removeSelectedFile(): void {
+    this.selectedFile = null;
+    this.selectedFilePreviewURL = '';
+    return;
   }
 
   // view preview pdf
@@ -81,6 +92,43 @@ export class AddMaterialsComponent implements OnInit {
       return;
     }
     this.zoomLevel -= 1;
+  }
+
+  // on submiting the form
+  async uploadMaterial(): Promise<void> {
+    try {
+      // checking image file
+      console.log(this.selectedFile, typeof(this.selectedFile));
+      if (this.selectedFile == null || undefined) {
+        this.toast.info('Please select the file')
+        return;
+      };
+      // this.updateFormData();
+      // this.loader.show();
+      // await this.bookServe.createBook(this.formData);
+      // this.toast.success('uploaded');
+      // this.router.navigate(['/materials'])
+    } catch (error: any) {
+      this.toast.error(error?.error?.message)
+      console.log(error);
+    } finally {
+      this.loader.hide();
+    }
+  }
+
+  // update form data
+  updateFormData(): void {
+    const formValues = this.createMaterialForm.value;
+    Object.entries(formValues).forEach(([key, value]: any) => {
+      if (Array.isArray(value)) {
+        value.forEach(v => {
+          this.formData.append(key, v);
+        })
+      } else {
+        this.formData.append(key, value);
+      }
+    })
+    this.formData.append('file', this.selectedFile);
   }
 
   ngOnInit(): void {
